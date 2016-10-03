cab2153cbeb48163479fd7d43bd83b658e5fa2eb
hamannjames <hamann.james87@gmail.com>
2016-10-03 10:34:58 -0700

    Header File
    Base header file, including:
    
    - language declarations
    - wp_head()
    - body classes
    - header container
    - nav container
    - logo container
    - site wrapper and content wrapper beginnings

---
+++ index.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--[if IE 6]>
+<html id="ie6" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 7]>
+<html id="ie7" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if IE 8]>
+<html id="ie8" <?php language_attributes(); ?>>
+<![endif]-->
+<!--[if !(IE 6) | !(IE 7) | !(IE 8)  ]><!-->
+<html <?php language_attributes(); ?>>
+<head>
+
+    <?php wp_head(); ?>
+
+</head>
+
+<body <?php body_class(); ?> >
+    <div id = "site-wrapper">
+        <div id = "site-container">
+
+            <div id = "header-wrapper">
+                <div id = "header-container">
+                    <header>
+                        <div id = "logo-container">
+                        </div>
+
+                        <div id = "nav-wrapper">
+                            <nav>
+                            </nav>
+                        </div>
+
+                    </header>
+                </div>
+            </div>
+
+            <div id = "content-wrapper">
+                <div id = "content-container">
